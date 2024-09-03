988c66569edd4c28f13bbeb14f439d2edcead66c
jackieng2000 <jackieng2000@gmail.com>
2024-09-03 15:00:21 +0800

    my update

--- bcre/settings.py
+++ bcre/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig',
+
 ]
 
 MIDDLEWARE = [
