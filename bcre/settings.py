1d80ab389e4b74fc9e571be4923366be837fe628
jackieng2000 <jackieng2000@gmail.com>
2024-09-04 12:31:58 +0800

    my update

--- bcre/settings.py
+++ bcre/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'bcre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bcre_prod',
+        'USER' : 'dbadmin',
+        'PASSWORD' : '1234',
+        'HOST' : 'localhost',
+
     }
 }
 
